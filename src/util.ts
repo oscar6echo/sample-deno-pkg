9c24baa60d0879f35ea6e79799b18006a9893038
Olivier Borderies <olivier.borderies@gmail.com>
2024-10-02 16:25:30 +0200

    Init

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 // works always
 import * as ac from "jsr:@cliffy/ansi@1.0.0-rc.5/colors";
 
-// works except when run as https://xyz
+// works except when run as https://xyz/main.ts
 // import * as ac from "@cliffy/ansi/colors";
 
 const color = {
